3c3c9fe04f5569eaa69b2b1a0541640dd4340e5c
manoj <manoj.sarwate@ssiedu.in>
2019-08-08 19:39:27 +0530

    08-08-19

--- src/Demo.java
+++ src/Demo.java
@@ -1,30 +1,61 @@
 public class Demo {
     
-    int x;          //instance (non-static)
-    static int y;   //class (static)
+    private int x, y;
     
-    //static method 
-    static void disp(){
-        //System.out.println(x);
-        System.out.println(y);
+    public Demo(){
+        this(5);
+        System.out.println("Zero Args");
+    }
+    public Demo(int p){
+        System.out.println("One Args");
     }
     
-    //non-static (using instance and class var both)
-    void show(){
-        System.out.println(x);
-        System.out.println(y);
-    }
     
+    public Demo(Demo tmp){
+        x=tmp.x;
+        y=tmp.y;
+    }
+    public Demo(int a, int b){
+        x=a; 
+        y=b;
+    }
+    public void show(){
+        System.out.println(x+","+y);
+    }
     
     public static void main(String[] args) {
 
-        Demo d1=new Demo();
-        Demo d2=new Demo();
-        d1.x=10;
-        d2.x=20;
-        y=25;
-        d2.show();
+        Demo d=new Demo();
         
-        Demo.disp();
+    
+        /*
+        Demo d2=new Demo(30,40);
+        Demo d3=new Demo(d2);
+        d1.show();
+        d2.show();
+        d3.show();
+        */
     }
 }
+/*
+1) Cons. is a method like body having same name as class.
+2) They do not have a return type.
+3) They may have an access modifier
+4) They may or may not have arguments.
+5) They can be overloaded also.
+6) They are implicitly called during object creation.
+    (matching args)
+7) When you do not write any constructor, java internally
+   provides a no-arg constructor for you, which is called
+   default constructor.
+8) Once you write your own constructor, default will lost.
+9) They are generally used for initialization purpose.
+10) constructor will be invoked once only on an object
+    where as you can call a method any times.
+11) java do not provide copy constructor but you can write
+    your own.
+    in place of copy constructor, java has given us a method
+    called clone.
+12) one constructor can call another constructor explicitly
+    using this keyword.
+*/
--- src/Test.java
+++ src/Test.java
@@ -1,31 +1,27 @@
 
 public class Test {
-    int a, b;   //non-static (instance)
-    static int x, y; //static (class)
+    
+    int x, y, z;
+                            
+                   //10    20
+    public Test(int p, int q){
+        x=p;
+        y=q;
+    }
+                //10      20     30
+    public Test(int a, int b, int c){
+        this(a,b);
+        z=c;
+    }
+    
+    void show(){
+        System.out.println(x+","+y+","+z);
+    }
     
     public static void main(String[] args) {
-        //instatiating the class Test thrice.
-        //each instantiation will create an instance(object)
-        Test t1=new Test();
-        Test t2=new Test();
-        Test t3=new Test();
         
-        t1.a=10;        t1.b=20;
-        t2.a=30;        t2.b=40;
-        t3.a=50;        t3.b=60;
-        System.out.println(t1.a+","+t1.b);
-        System.out.println(t2.a+","+t2.b);
-        System.out.println(t3.a+","+t3.b);
-        System.out.println("______________________");
-        t1.x=100;
-        t2.x=200;
-        t3.x=300;
-        
-        System.out.println(t1.x);
-        System.out.println(t2.x);
-        System.out.println(t3.x);
-        
-        
+        Test t1=new Test(10,20,30);
+        t1.show();
     }
 
 }
